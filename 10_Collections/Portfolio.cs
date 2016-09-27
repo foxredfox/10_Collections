d6ea951036e0918fcc33832c38edaaab74ab9ade
black <black@DESKTOP-IAP7KVH>
2016-09-27 08:44:19 +0200

    Add project files.

---
+++ 10_Collections/Portfolio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Collections
+{
+    class Portfolio
+    {
+        private List<Asset> assets = new List<Asset>();
+
+        public Portfolio(){}
+
+        public Portfolio(List<Asset> assets)
+        {
+            this.assets = assets;
+        }
+
+        public double GetTotalValue()
+        {
+            return assets.Sum(asset => asset.GetValue());
+        }
+
+        public void AddAsset(Asset asset)
+        {
+            assets.Add(asset);
+        }
+
+        public List<Asset> GetAssets()
+        {
+
+        }
+
+        public Asset GetAssetByName()
+        {
+
+        }
+
+    }
+}
---
+++ 10_Collections/SavingsAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Portfolio
+{
+    class SavingsAccount : Asset
+    {
+        public double InterestRate{ get; set;}
+        public double Value{ get; set;}
+        public String AccountID{ get; set;}
+
+        public SavingsAccount(){}
+        public SavingsAccount(String AccountID, double Value, double InterestRate)
+        {
+            this.AccountID    = AccountID;
+            this.Value        = Value;
+            this.InterestRate = InterestRate;
+        }
+        
+        public double GetValue()
+        {
+            return Value;
+        }
+
+        public override String ToString()
+        {
+            return 
+                "SavingsAccount[value="+ Value.ToString("F1").Replace(',', '.') +
+                ",interestRate="+ InterestRate.ToString("F1").Replace(',', '.')+ "]";
+        }
+
+        public void ApplyInterest()
+        {
+            Value += Value * (InterestRate / 100);
+        }
+
+        public static double TotalValue(Asset[] accounts)
+        {
+            return accounts.Sum(account => account.GetValue());
+        }
+    }
+}
